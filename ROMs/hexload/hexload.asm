c5d1e6ce42e682f6f4384e28c5f6a560be378a33
Phillip Stevens <phillip.stevens@gmail.com>
2017-06-29 00:40:42 +1000

    adapt hexload to z88dk

--- ROMs/hexload/hexload.asm
+++ ROMs/hexload/hexload.asm
@@ -1,143 +1,97 @@
-SECTION INIT
-ORG     $F800 ; 0xFFFF - 2048
+SECTION     CODE
 
-start:      ;di              ; disable interrupts
-            ld hl, $FFFF    ; set new stack location
-            ld sp, hl       ; to $FFFF
-            ld a, $03
-            out ($80),a     ; ACIA master reset
-            ld a, $96       ; Initialize ACIA
-            out ($80),a
-            ei
-            ;di
-
-
-mainf:      ld hl, initstr
+main:
+            ld hl, initstr
             call print
-
-waitcol:    call RX         ; wait for ':'
-            ld a, l
+waitcolon:
+            rst 10H         ; wait for Rx ':'
             cp ':'
-            jp nz, waitcol
-            ld ix, 0        ; reset ix to compute checksum
+            jr nz, waitcolon
+            ld hl, 0        ; reset hl to compute checksum
             call readbyte   ; read byte count
-            ld b, h         ; store it in bc
-            ld c, l         ;
+            ld b, a         ; store it in bc
             call readbyte   ; read upper byte of address
-            ld d, l         ; store in d
+            ld d, a         ; store in d
             call readbyte   ; read lower byte of address
-            ld e, l         ; store in e
+            ld e, a         ; store in e
             call readbyte   ; read record type
-            ld a, l         ; store in a
             cp 01           ; check if record type is 01 (end of file)
             jr z, endload
             cp 00           ; check if record type is 00 (data)
             jr nz, invtype  ; if not, error
-
-readdata:   call readbyte
-            ld a, l
+readdata:
+            call readbyte
             ld (de), a
             inc de
-            dec bc
-            ld a, 0         ; check if bc==0
-            or b
-            or c
-            cp 0
-            jr nz, readdata ; if not, loop
+            djnz readdata   ; if not, loop
 
-            ;ld a, '|'
-            ;call TX
             call readbyte   ; read checksum
-            ld a, ixl       ; lower byte of ix should be 0
-            cp 0
+            ld a, l         ; lower byte of hl should be 0
+            or a
             jr nz, badck
 
-            ld a, '*'
-            call TX
-            jp waitcol
+            ld a, '#'       ; "#" per line loaded
+            rst 8H          ; Tx byte
+            jr waitcolon
 
-endload:    call readbyte   ; read last checksum (not used)
+endload:
+            call readbyte   ; read last checksum (not used)
+            ld a, l         ; lower byte of hl checksum should be 0
+            or a
+            jr nz, badck    ; non zero, we have an issue
             ld hl, loadokstr
             call print
-            ld hl, $8080
-            jp (hl)
-            ;jp hang
-
-invtype:    ld hl, invalidtypestr
-            call print
-            jp hang
-
-badck:      ld hl, badchecksumstr
-            call print
-            jp hang
-
-hang:
-            nop
-            jp hang
-
-TX:         push af
-txbusy:     in a,($80)          ; read serial status
-            bit 1,a             ; check status bit 1
-            jr z, txbusy        ; loop if zero (serial is busy)
-            pop af
-            out ($81), a        ; transmit the character
             ret
 
-RX:
-            push af
-waitch:     ;in a, ($80)
-            ;bit 0, a
-            ;jr z, waitch
-            ;in a, ($81)
-            ;ld h, 0
-            rst $10
-            ld l, a
-            ;call TX
-            pop af
+invtype:
+            ld hl, invalidtypestr
+            call print
             ret
+
+badck:
+            ld hl, badchecksumstr
+            call print
+            ret
+
+readbyte:                   ; Returns byte in a, checksum in hl
+            push bc
+            rst 10H         ; Rx byte
+            sub '0'
+            cp 10
+            jr c, rnib2     ; if a<10 read the second nibble
+            sub 7           ; else subtract 'A'-'0' (17) and add 10
+rnib2:
+            rlca            ; shift accumulator left by 4 bits
+            rlca
+            rlca
+            rlca
+            ld c, a         ; temporary store the first nibble in c
+            rst 10H         ; Rx byte
+            sub '0'
+            cp 10
+            jr c, rend      ; if a<10 finalize
+            sub 7           ; else subtract 'A' (17) and add 10
+rend:
+            or c            ; assemble two nibbles into one byte in a
+            ld b, 0         ; add the byte read to hl (for checksum)
+            ld c, a
+            add hl, bc
+            pop bc
+            ret             ; return the byte read in a
 
 print:
             ld a, (hl)
             or a
             ret z
-            call TX
+            rst 8H          ; Tx byte
             inc hl
-            jp print
+            jr print
 
-readbyte:
-            push af
-            push de
-            call RX
-            ld a, l
-            sub '0'
-            cp 10
-            jr c, rnib2    ; if a<10 read the second nibble
-            sub 7          ; else subtract 'A'-'0' (17) and add 10
-rnib2:      ld d, a        ; temporary store the first nibble in d
-            call RX
-            ld a, l
-            sub '0'
-            cp 10
-            jr c, rend     ; if a<10 finalize
-            sub 7          ; else subtract 'A' (17) and add 10
-rend:       ld e, a        ; temporary store the second nibble in e
-            sla d          ; shift register d left by 4 bits
-            sla d
-            sla d
-            sla d
-            or d
-            pop de
-            ld h, 0
-            ld l, a
-            pop af
-            push bc         ; add the byte read to ix (for checksum)
-            ld b, 0
-            ld c, l
-            add ix, bc
-            pop bc
-            ret
+SECTION             DATA
 
-initstr:            DEFM "HEX LOADER by Filippo Bergamasco",10,13,0
-invalidtypestr:     DEFM 10,13,"INV TYP",0
-badchecksumstr:     DEFM 10,13,"BAD CHK",0
-loadokstr:          DEFM 10,13,"OK",0
+initstr:            DEFM "HEX LOADER by Filippo Bergamasco"
+                    DEFM " & feilipu for z88dk"
+                    DEFM 10,13,":",0
+invalidtypestr:     DEFM 10,13,"Invalid Type",10,13,0
+badchecksumstr:     DEFM 10,13,"Bad Checksum",10,13,0
+loadokstr:          DEFM 10,13,"Done",10,13,0
---
+++ ROMs/hexload/hexload_map.asm
@@ -0,0 +1,18 @@
+;==============================================================================
+; Contents of this file are copyright Phillip Stevens
+;
+; You have permission to use this file,
+; but please include an acknowledgement.
+;
+; https://github.com/feilipu/
+;
+; https://feilipu.me/
+;
+
+SECTION     CODE
+ORG         0x8900
+
+SECTION     DATA
+ORG         -1
+
+;==============================================================================
