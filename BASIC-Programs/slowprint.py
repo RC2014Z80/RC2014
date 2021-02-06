bea6c5b7147184204f7548134d0290b8b28b480e
Phillip Stevens <phillip.stevens@gmail.com>
2021-02-06 16:45:52 +1100

    revert to CR for slowprint.py

--- BASIC-Programs/slowprint.py
+++ BASIC-Programs/slowprint.py
@@ -6,6 +6,6 @@ for line in sys.stdin:
     for ch in line:
         sys.stdout.write( ch );
         sleep(0.01);
-    sys.stdout.write('\n');
+    sys.stdout.write('\r');
     sys.stdout.flush();
     sleep(0.1);
